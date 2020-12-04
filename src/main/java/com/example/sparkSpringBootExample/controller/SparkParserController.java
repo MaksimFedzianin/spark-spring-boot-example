fd174c78940a4b7f3f433ec622794eb4ca12ac5b
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2020-12-04 17:59:28 +0300

    Added hello world endpoint

--- src/main/java/com/example/sparkSpringBootExample/controller/SparkParserController.java
+++ src/main/java/com/example/sparkSpringBootExample/controller/SparkParserController.java
@@ -3,6 +3,7 @@ package com.example.sparkSpringBootExample.controller;
 import com.example.sparkSpringBootExample.service.BasicElementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class SparkParserController {
     public ResponseEntity<String> parse() {
         basicElementService.save();
         return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping(value = "/hello")
+    public ResponseEntity<?> getHelloWorld(){
+        return ResponseEntity.ok("Hello world");
     }
 }
